b328209222017f754e0f10019797beead93348b7
Omar-RLCS <elchapocoldzera@gmail.com>
2024-08-11 20:37:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglePaper() {
+    var paper = document.getElementById("paper");
+    if (paper.style.display === "none") {
+        paper.style.display = "block";
+    } else {
+        paper.style.display = "none";
+    }
+}
